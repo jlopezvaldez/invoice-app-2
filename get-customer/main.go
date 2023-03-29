74c0c2d9a137b615581a230393a7b027c2fe2f7a
jonathan <jlopezv0324@gmail.com>
2023-03-29 15:26:08 -0700

    commit

--- get-customer/main.go
+++ get-customer/main.go
@@ -1,6 +1,15 @@
 package main
 
-import "github.com/aws/aws-lambda-go/lambda"
+import (
+	"encoding/json"
+	"log"
+	"net/http"
+	"os"
+	"regexp"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+)
 
 type customer struct {
 	PhoneNumber   string `json:phone`
@@ -9,14 +18,53 @@ type customer struct {
 	ZipCode       string `json:zip`
 }
 
-func showCustomer() (*customer, error) {
-	//fetch a customer from dynamo using phoneNumber
-	cus, err := getItem("8888888888")
-	if err != nil {
-		return nil, err
+var errorLogger = log.New(os.Stderr, "ERROR", log.Llongfile)
+var numberRegex = regexp.MustCompile(`[0-9]{10}`)
+
+func showCustomer(req events.APIGatewayProxyRequest) (events.APIGatewayProxyResponse, error) {
+	//check if request is phone number
+	phoneNumber := req.QueryStringParameters["phoneNumber"]
+	if !numberRegex.MatchString(phoneNumber) {
+		return clientError(http.StatusBadRequest)
 	}
 
-	return cus, nil
+	//fetch a customer from dynamo using phoneNumber
+	cus, err := getItem(phoneNumber)
+	if err != nil {
+		return serverError(err)
+	}
+
+	if cus == nil {
+		return clientError(http.StatusNotFound)
+	}
+
+	js, err := json.Marshal(cus)
+	if err != nil {
+		return serverError(err)
+	}
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusOK,
+		Body:       string(js),
+	}, nil
+}
+
+// helper function for errors
+func serverError(err error) (events.APIGatewayProxyResponse, error) {
+	errorLogger.Println(err.Error())
+
+	return events.APIGatewayProxyResponse{
+		StatusCode: http.StatusInternalServerError,
+		Body:       http.StatusText(http.StatusInternalServerError),
+	}, nil
+}
+
+// helper function for errors
+func clientError(status int) (events.APIGatewayProxyResponse, error) {
+	return events.APIGatewayProxyResponse{
+		StatusCode: status,
+		Body:       http.StatusText(status),
+	}, nil
 }
 
 func main() {
